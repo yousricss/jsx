a4d83b94d6d6cc43d1a336c4ffb4d0c84b5d89eb
yousricss <yousri_bahloul@yahoo.fr>
2021-05-10 00:12:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import image from '../src/image/imageInSrc.jpg';
+
+function App() {
+  return (
+    <div className="container" >
+      <h1 class="title red">Yousri</h1>
+      <br/>
+      <img src={image} alt=""/>
+      <br/>
+      <img src="/image/imageInPublic.jpg" alt=""/>
+      <br/>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/BS9g6Z3lbBU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    
+      
+      
+    </div>
+  );
+}
+
+export default App;
